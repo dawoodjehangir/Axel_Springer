417572777ed22f7fe0b5c3cd10afb11f0f66760d
dawood <dawood.jehangir@gmail.com>
2023-12-18 21:30:31 +0000

    basic cleanup + cors + react-app url added in constants

--- backend/src/controllers/joke.js
+++ backend/src/controllers/joke.js
@@ -1,4 +1,5 @@
 const { getJokeFromChuck } = require("../services/joke.js");
+const { REACTAPP } = require("../services/joke.js");
 
 /**
  * Communicating with the Chuck Norris API and sending a response back
@@ -8,9 +9,9 @@ const getJoke = async (request, response) => {
     const randomJoke = await getJokeFromChuck();
     const headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "http://localhost:3000"
+      "Access-Control-Allow-Origin": REACTAPP,
     };
-    // "Access-Control-Allow-Origin": "http://127.0.0.1:5500",
+
     response.set(headers);
     response.status(200).json({ Id: randomJoke.id, Joke: randomJoke.value });
   } catch (error) {
--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,7 +1,9 @@
 const PORT = 8000;
 const CHUCKAPI = "https://api.chucknorris.io/jokes/random";
+const REACTAPP = "http://localhost:3000";
 
 module.exports = {
   PORT,
   CHUCKAPI,
+  REACTAPP,
 };
