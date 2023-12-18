d61fe5a8646b5b0d991a65dc4a34f24ab6b01836
dawood <dawood.jehangir@gmail.com>
2023-12-18 00:51:26 +0000

    someUpdates

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,45 @@
+import "./App.css";
+import JokeContainer from "./components/JokeContainer";
 
-import './App.css';
-
+// https://pngimg.com/uploads/chuck_norris/small/chuck_norris_PNG13.png
 function App() {
+  const fetchJoke = () => {
+    return "This is some Joke";
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <MidDivider />
+      <JokeContainer />
+      <button onClick={fetchJoke}>Humor Me</button>
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <div className="ChuckHeader">
+        <img
+          src="https://pngimg.com/uploads/chuck_norris/small/chuck_norris_PNG13.png"
+          alt="Chuck Norris"
+          className="ChuckFace"
+        />
+        <div>
+          <span className="ChuckOrangeText">Chuck Norris</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function MidDivider() {
+  return (
+    <>
+      <div className="Divider">
+        <span className="DividerText">Wanna get a Random Joke?</span>
+      </div>
+    </>
   );
 }
 
---
+++ react-app/src/components/JokeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JokeContainer = ({ joke }) => {
+  return (
+    <div className="joke-container">
+      <p>This is something</p>
+    </div>
+  );
+};
+
+export default JokeContainer;
